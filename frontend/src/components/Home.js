4eff946b98767593eaf33b45077afcf276745285
Kai-Barker <241065@virtualwindow.co.za>
2025-03-11 22:36:46 +0200

    Made a large card, updated the cards to read in object data

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import HeroSection from './Hero_section';
-import Card from './SmallCard';
+import SmallCard from './SmallCard';
+import LargeCard from './Large_card';
+
+const driver = {
+    name: 'Lewis Hamilton',
+    isCompeting: true,
+    lastRace: '2024 Abu Dhabi Grand Prix',
+    nationality: 'British',
+    wins: 105,
+    polePositions: 104,
+    bestChampPos:1,
+    championshipWins:7,
+    podiums:202,
+    numRaces: 356,
+    numSeasons: 17,
+    winrate: 29.5,
+    currentConstructor: {
+        name:'Scuderia Ferrari',
+        isCompeting: true,
+        lastRace:'2024 Abu Dhabi Grand Prix',
+        nationality: 'Italian',
+        wins:247,
+        polePositions:253,
+        bestChampPos:1,
+        championshipWins:15,
+        podiums:202,
+        numRaces: 356,
+        numSeasons: 17,
+        numDrivers: 114
+    }
+}
 
 function Home(){
+    console.log(driver);
     return (
-        <div style={{ marginLeft: '20%', top:'0px', height: '100vh' }}>
+        <div style={{ marginLeft: '20%', top:'0px' }}>
             .
+            
             <HeroSection />
-            <Card />
+            <div className="d-flex flex-row" style={{ gap: '5vh' }}>
+                <SmallCard driver={driver}/>
+                <LargeCard />
+            </div>
+            <div className="d-flex flex-row" style={{ gap: '5vh' }}>
+                <LargeCard />
+                <SmallCard driver={driver.currentConstructor}/>
+            </div>
         </div>
     );
 };
--- frontend/src/components/SmallCard.js
+++ frontend/src/components/SmallCard.js
@@ -4,34 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Card.css'; // Import the CSS file
 import bgTexture from '../images/MetalTexture.jpeg';
 
-function SmallCard(){
+function SmallCard({driver}){
+    if (!driver) {
+        console.log("Driver not found")
+        return null;
+    }
     return (
         <Card style={{ borderRadius: '15px 0px 15px 0px', border: '1px solid #ccc', width: '25%', marginTop:'8vh',borderLeft: '5px solid #FF1801',
             borderTop: '5px solid #FF1801', boxShadow: '2px 2px 5px 0px rgb(255, 255, 255)', backgroundImage: `url(${bgTexture})`,backgroundColor: 'rgba(255, 255, 255, 0.7)', backgroundBlendMode: 'overlay', backgroundSize: 'cover' }}>
             <Card.Body>
-                <Card.Title className="custom-card-title" id='driverName' >Lewis Hamilton</Card.Title>
+                <Card.Title className="custom-card-title" id='driverName' >{driver.name}</Card.Title>
                 <div style={{textAlign: 'left'}}>
                 <Card.Text id='competeStatus' style={{fontSize:'small', marginBottom:'0'}}>
-                    Is Currently Competing
+                    {driver.isCompeting ? "Is Currently Competing" : "Not Competing"}
                 </Card.Text>
                 <Card.Text id='lastRace' style={{fontSize:'small', marginBottom:'0'}}>
-                    Last Race: 2024 Abu Dhabi Grand Prix
+                    {'Last Race: '+driver.lastRace}
                 </Card.Text>
                 <hr style={{margin: '5px', marginLeft: '-6%', padding:'4px', color:'#fff', backgroundColor:'#FF1801', width:'112.5%', opacity:'100'}}/>
                 <Card.Text className='statText'>
-                    Nationality: British
+                    {'Nationality: '+driver.nationality}
                 </Card.Text>
                 <Card.Text className='statText'>
-                    Wins: 105
+                    {'Wins: '+driver.wins}
                 </Card.Text>
                 <Card.Text className='statText'>
-                    Pole Positions: 104
+                    {'Pole Positions: '+driver.polePositions}
                 </Card.Text>
                 <Card.Text className='statText'>
-                    Best Championship Position: 1
+                   {'Best Championship Position: '+driver.bestChampPos}
                 </Card.Text>
                 <Card.Text className='statText'>
-                    WDC Wins: 7
+                    {'Championship Wins: '+driver.championshipWins}
                 </Card.Text>
                 </div>
             </Card.Body>
