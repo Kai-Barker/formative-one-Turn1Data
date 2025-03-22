af267ab894fcf5d93799f8f8198f037130243512
Kai-Barker <241065@virtualwindow.co.za>
2025-03-22 21:15:17 +0200

    added api fetches for more data

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -37,41 +37,6 @@ const driver = {
 
 
 function Home() {
-  const [drivers,setDrivers] = useState([]);
-  const fetchDrivers = async () => {
-    const allDrivers = [];
-    let totalDrivers = 0;
-    let limit = 100;
-    let offset = 0;
-    try {
-      const response = await axios.get("https://api.jolpi.ca/ergast/f1/drivers/",{
-        params: { limit, offset }, // Fetch more than the apis default 30. Maxes out at 100 so ive gotta fetch numerous times
-      });
-      totalDrivers = response.data.MRData.total;
-      console.log(totalDrivers);
-      allDrivers=response.data.MRData.DriverTable.Drivers;
-      
-      // while (allDrivers.length<totalDrivers) {
-      //   offset+=limit;
-      //   const nextResponse = await axios.get("https://api.jolpi.ca/ergast/f1/drivers/", {
-      //     params: { limit, offset },
-      //   });
-      //   allDrivers = allDrivers.concat(nextResponse.data.MRData.DriverTable.Drivers);
-      // }
-      setDrivers(allDrivers);
-      
-    } catch (error) {
-      console.error("Error fetching data:");
-    }
-  };
-  useEffect(() => {
-    fetchDrivers();
-    
-  }, []);
-  useEffect(() => {
-    console.log("Updated drivers:", drivers);
-  }, [drivers]);
-
   return (
     <div style={{ marginLeft: "20%", top: "0px" }}>
       .
