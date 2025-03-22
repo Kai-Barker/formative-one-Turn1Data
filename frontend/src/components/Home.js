b9a8372c015a53f228b4a1d1cf8adebb034fa9fa
Kai-Barker <241065@virtualwindow.co.za>
2025-03-22 18:11:46 +0200

    Added some API stuff, hit the limit >:(

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import HeroSection from "./Hero_section";
 import SmallCard from "./SmallCard";
 import LargeCard from "./Large_card";
 import DataSelectors from "./Data_selectors";
+import axios from "axios";
 
 const driver = {
   name: "Lewis Hamilton",
@@ -33,8 +34,44 @@ const driver = {
   },
 };
 
+
+
 function Home() {
-  console.log(driver);
+  const [drivers,setDrivers] = useState([]);
+  const fetchDrivers = async () => {
+    const allDrivers = [];
+    let totalDrivers = 0;
+    let limit = 100;
+    let offset = 0;
+    try {
+      const response = await axios.get("https://api.jolpi.ca/ergast/f1/drivers/",{
+        params: { limit, offset }, // Fetch more than the apis default 30. Maxes out at 100 so ive gotta fetch numerous times
+      });
+      totalDrivers = response.data.MRData.total;
+      console.log(totalDrivers);
+      allDrivers=response.data.MRData.DriverTable.Drivers;
+      
+      // while (allDrivers.length<totalDrivers) {
+      //   offset+=limit;
+      //   const nextResponse = await axios.get("https://api.jolpi.ca/ergast/f1/drivers/", {
+      //     params: { limit, offset },
+      //   });
+      //   allDrivers = allDrivers.concat(nextResponse.data.MRData.DriverTable.Drivers);
+      // }
+      setDrivers(allDrivers);
+      
+    } catch (error) {
+      console.error("Error fetching data:");
+    }
+  };
+  useEffect(() => {
+    fetchDrivers();
+    
+  }, []);
+  useEffect(() => {
+    console.log("Updated drivers:", drivers);
+  }, [drivers]);
+
   return (
     <div style={{ marginLeft: "20%", top: "0px" }}>
       .
